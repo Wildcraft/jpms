029ff0af142e4c72deb1e09fe4e44484ee5a7222
Nagarajan Sivaraman (NAS) <Nagarajan.Sivaraman@softwareag.com>
2017-09-03 19:05:17 +0530

    initial commit

---
+++ root/01_greetings/src/main/java/io/github/wildcraft/greetings/Greetings.java
@@ -0,0 +1,13 @@
+package io.github.wildcraft.greetings;
+
+public class Greetings {
+
+    private void print () {
+        System.out.println("Hello world!!");
+    }
+
+    public static void main(String[] args) {
+        Greetings greetings = new Greetings();
+        greetings.print();
+    }
+}
